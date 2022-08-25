a5a3ebfadba46fbaf4bfc1f5b271daab1939110f
Martin <escorpmartin97@gmail.com>
2022-08-25 19:05:39 -0300

    style: define estilos globais

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,12 +7,32 @@ import { createGlobalStyle } from 'styled-components';
     box-sizing: 0;
   }
 
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${({theme}) => theme['base-text']};
+  }
+  
   :focus {
     outline: 0;
     box-shadow: 0 0 0 2px;
   }
  
+  h1 {
+    color: ${({theme}) => theme['base-title']};
+    line-height: 130%;
+    font-family: 'Baloo 2', cursive;
+  }
+
+  p {
+    color: ${({theme}) => theme['base-subtitle']};
+  }
+
+  button {
+    color: ${({theme}) => theme['base-button']};
+  }
+
   body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
     font-size: 1rem;
     font-weight: 400;
   }
