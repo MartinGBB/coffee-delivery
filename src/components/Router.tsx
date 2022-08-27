1327b9c93c5e33b3def28b8bf2886ea17e98bb8b
Martin <escorpmartin97@gmail.com>
2022-08-26 21:41:49 -0300

    fix: coloca retorno no componente de rotas

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 
 export function Router() {
-  ;<Routes>
-    <Route path="/" element={<h1>Hello world</h1>} />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Hello world</h1>} />
+    </Routes>
+  )
 }
