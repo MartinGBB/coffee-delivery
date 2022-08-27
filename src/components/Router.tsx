dfee6b589348665cf1bad265e5219793e1e039b8
Martin <escorpmartin97@gmail.com>
2022-08-26 22:10:29 -0300

    feat: configura rotas dos componentes e cria layout para a pagina

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,9 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from '../layouts/DefaultLayout'
+import { Home } from '../pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Hello world</h1>} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<h1>Checkout</h1>} />
+        <Route path="/success" element={<h1>Success</h1>} />
+      </Route>
     </Routes>
   )
 }
