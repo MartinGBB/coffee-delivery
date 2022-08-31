e76d057ce8b97e4e1cc8ca092c08615bbb99ab03
Martin <escorpmartin97@gmail.com>
2022-08-30 21:47:21 -0300

    styles: desativa scroll x

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme['base-text']};
+    overflow-x: hidden;
   }
   
   :focus {
