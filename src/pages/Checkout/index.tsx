1bc07c18f4c4b8215620df33cb1bc0dbcfd196ab
Martin <escorpmartin97@gmail.com>
2022-09-05 21:47:09 -0300

    feat: adiciona campos do formulario do checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,24 @@
+import { CheckoutContainer, FormContainer } from './styles'
+
 export function Checkout() {
   return (
-    <div>
-      <h1>Checkout</h1>
-    </div>
+    <CheckoutContainer>
+      <FormContainer>
+        <form action="">
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </form>
+      </FormContainer>
+      <SelectCoffee>
+
+      </SelectCoffee>
+
+    </CheckoutContainer>
   )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const CheckoutContainer = styled.div`
+  margin: 6.5rem 10rem 0 10rem;
+`
+export const FormContainer = styled.div``
+
+export const SelectCoffee = styled.div``
