339e886db6a2d945434322dc6dcc6f2d3c886494
Martin <escorpmartin97@gmail.com>
2022-09-07 17:47:52 -0300

    style: configura titulos do primeiro cart

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -13,10 +13,30 @@ export const AddressContainer = styled.div`
     font-weight: 700;
     font-size: 1.125rem;
     margin-bottom: 0.9375rem;
+    color: ${({ theme }) => theme['base-subtitle']};
   }
 `
 
-export const FormContainer = styled.div``
+export const FormContainer = styled.div`
+  background: ${({ theme }) => theme['base-card']};
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+
+  div {
+    margin-bottom: 2rem;
+
+    & h1 {
+      color: ${({ theme }) => theme['base-subtitle']};
+      font-weight: 400;
+      font-size: 1rem;
+    }
+    & p {
+      color: ${({ theme }) => theme['base-text']};
+      font-size: 0.875rem;
+    }
+  }
+`
 
 export const ContainerPagament = styled.div``
 
