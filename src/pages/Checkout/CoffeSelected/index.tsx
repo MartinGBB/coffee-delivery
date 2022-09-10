04229daa54dfde0376204dabda76ec5c0547be8c
Martin <escorpmartin97@gmail.com>
2022-09-10 16:31:59 -0300

    feat: configura novo componente para renderizar informacoes

--- src/pages/Checkout/CoffeSelected/index.tsx
+++ src/pages/Checkout/CoffeSelected/index.tsx
@@ -1,9 +1,30 @@
-import { SelectedContainer } from './styles'
+import { productsData } from '../../../utils/productsData'
+import { ButtonsAddToCart } from '../../Home/components/Products/styles'
+import { Product, ProductsContainer, SelectedContainer } from './styles'
 
 export function CoffeeSelected() {
   return (
     <SelectedContainer>
-      <h1>Coffee Selected</h1>
+      {productsData.map((productCart) => {
+        return (
+          <ProductsContainer key={productCart.id}>
+            <Product>
+              <img src={productCart.image} alt={productCart.name} />
+              <h1>{productCart.name}</h1>
+              <ButtonsAddToCart>
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </ButtonsAddToCart>
+              <button>REMOVER</button>
+            </Product>
+
+            <span>
+              R$ <span>{productCart.price}</span>
+            </span>
+          </ProductsContainer>
+        )
+      })}
     </SelectedContainer>
   )
 }
--- src/pages/Checkout/CoffeSelected/styles.ts
+++ src/pages/Checkout/CoffeSelected/styles.ts
@@ -5,5 +5,14 @@ export const SelectedContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 2.5rem;
-  border-radius: 6px;
+  border-radius: 6px 44px;
+`
+
+export const ProductsContainer = styled.div`
+  
+`
+
+
+export const Product = styled.div`
+
 `
