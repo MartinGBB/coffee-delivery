87df2ac8805093eb37f732fb93c7970abbf719a1
Martin <escorpmartin97@gmail.com>
2022-09-11 13:41:26 -0300

    feat: adiciona componente de entrega

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,9 +1,22 @@
-import { SuccessContainer } from './styles'
+import { AddressOrder, InfoContainer, SuccessContainer } from './styles'
 
 export function Success() {
   return (
     <SuccessContainer>
-      <h1>Success</h1>
+      <div>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </div>
+
+      <InfoContainer>
+        <AddressOrder>
+          <div>
+            <span>Entrega em</span>
+            <span>Rua João Daniel Martinelli, 102</span>
+          </div>
+          <span>Farrapos - Porto Alegre, RS</span>
+        </AddressOrder>
+      </InfoContainer>
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -5,11 +5,47 @@ export const SuccessContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: 1fr 2fr;
-  row-gap: 0.75rem;
+  row-gap: 2.25rem;
   column-gap: 2rem;
 
   @media (max-width: 1024px) {
     margin: 9rem 2rem 0 2rem;
     gap: 0;
+  }
+
+  > div {
+    grid-column: 1;
+
+    h1 {
+      color: ${({ theme }) => theme['yellow-dark']};
+      font-weight: 800;
+      font-size: 2rem;
+    }
+
+    p {
+      color: ${({ theme }) => theme['base-subtitle']};
+      font-weight: 400;
+      font-size: 1.25rem;
+    }
+  }
+`
+
+export const InfoContainer = styled.div`
+  grid-column: 1;
+  grid-row: 2;
+
+  /* display: grid;
+  grid-template-columns: 2rem 1fr;
+  grid-template-rows: repeat(2, 1fr); */
+`
+
+export const AddressOrder = styled.div`
+  div {
+    display: flex;
+    gap: 0.2rem;
+
+    & span:nth-child(2) {
+      font-weight: 700;
+    }
   }
 `
