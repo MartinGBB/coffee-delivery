72adc2fdeb8b37e383350cee713703bddfcfc563
Martin <escorpmartin97@gmail.com>
2022-09-18 23:20:51 -0300

    refator: separa componente de banner e separa estilos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,60 +1,12 @@
-import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
-import coffeBanner from '../../assets/coffeBanner.svg'
 import { productsData } from '../../utils/productsData'
+import { Banner } from './components/Banner'
 import { Products } from './components/Products'
-import {
-  BannerContainer,
-  TitleBanner,
-  ContentBanner,
-  HomeContainer,
-  DescriptionBanner,
-  ProductContainer,
-  IconContainer,
-} from './styles'
+import { HomeContainer, ProductContainer } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
-      <BannerContainer>
-        <ContentBanner>
-          <div>
-            <TitleBanner>
-              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-              <p>
-                Com o Coffee Delivery você recebe seu café onde estiver, a
-                qualquer hora
-              </p>
-            </TitleBanner>
-            <DescriptionBanner>
-              <p>
-                <IconContainer iconsColors="yellowDark">
-                  <ShoppingCart size={22} weight="fill" />
-                </IconContainer>
-                Compra simples e segura
-              </p>
-              <p>
-                <IconContainer iconsColors="black">
-                  <Package size={22} weight="fill" />
-                </IconContainer>
-                Embalagem mantém o café intacto
-              </p>
-              <p>
-                <IconContainer iconsColors="yellowLight">
-                  <Timer size={22} weight="fill" />
-                </IconContainer>
-                Entrega rápida e rastreada
-              </p>
-              <p>
-                <IconContainer iconsColors="purple">
-                  <Coffee size={22} weight="fill" />
-                </IconContainer>
-                O café chega fresquinho até você
-              </p>
-            </DescriptionBanner>
-          </div>
-          <img src={coffeBanner} alt="" />
-        </ContentBanner>
-      </BannerContainer>
+      <Banner />
       <ProductContainer>
         <h1>Nossos cafés</h1>
         <div>
