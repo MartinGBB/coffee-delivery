507c5a34f2d506d306191916706ecac2bcc15f73
Martin <escorpmartin97@gmail.com>
2022-09-19 12:05:42 -0300

    refator: separa componente de formulario

---
+++ src/pages/Checkout/FormPayment/index.tsx
@@ -0,0 +1,77 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+import {
+  AddressContainer,
+  ContainerPagament,
+  FormContainer,
+  SelectPagament,
+} from './styles'
+
+export function FormPayment() {
+  return (
+    <div>
+      <AddressContainer>
+        <h1>Complete seu pedido</h1>
+        <FormContainer>
+          <div>
+            <MapPinLine size={22} />
+            <h1>Endereço de Entrega</h1>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+          </div>
+          <form action="">
+            <div>
+              <input required type="text" placeholder="CEP" />
+              <input required type="text" placeholder="Rua" />
+            </div>
+            <div>
+              <input required type="text" placeholder="Número" />
+              <label>
+                <input type="text" placeholder="Complemento" />
+                Opcional
+              </label>
+            </div>
+            <div>
+              <input required type="text" placeholder="Bairro" />
+              <input required type="text" placeholder="Cidade" />
+              <input required type="text" placeholder="UF" />
+            </div>
+          </form>
+        </FormContainer>
+      </AddressContainer>
+
+      <ContainerPagament>
+        <div>
+          <CurrencyDollar size={22} />
+          <h1>Pagamento</h1>
+          <p>
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </p>
+        </div>
+        <SelectPagament>
+          <label htmlFor="debitCard">
+            <input id="debitCard" name="pagament" type="radio" />
+            <CreditCard size={16} />
+            CARTÃO DE CRÉDITO
+          </label>
+
+          <label htmlFor="creditCard">
+            <input id="creditCard" name="pagament" type="radio" />
+            <Bank size={16} />
+            CARTÃO DE DÉBITO
+          </label>
+
+          <label htmlFor="cash">
+            <input id="cash" name="pagament" type="radio" />
+            <Money size={16} />
+            DINHEIRO
+          </label>
+        </SelectPagament>
+      </ContainerPagament>
+    </div>
+  )
+}
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,21 +1,11 @@
-import {
-  Bank,
-  CreditCard,
-  CurrencyDollar,
-  MapPinLine,
-  Money,
-} from 'phosphor-react'
 import { useNavigate } from 'react-router-dom'
 import { productsOrder } from '../../utils/productsData'
 import { ConfirmOrder } from './ConfirmOrder'
+import { FormPayment } from './FormPayment'
 import {
-  AddressContainer,
   CheckoutContainer,
   ConfirmOrden,
-  ContainerPagament,
-  FormContainer,
   SelectedContainer,
-  SelectPagament,
   TotalContainer,
 } from './styles'
 
@@ -25,65 +15,10 @@ export function Checkout() {
   function confirmOrder() {
     navigate('/success')
   }
+
   return (
     <CheckoutContainer>
-      <AddressContainer>
-        <h1>Complete seu pedido</h1>
-        <FormContainer>
-          <div>
-            <MapPinLine size={22} />
-            <h1>Endereço de Entrega</h1>
-            <p>Informe o endereço onde deseja receber seu pedido</p>
-          </div>
-          <form action="">
-            <div>
-              <input required type="text" placeholder="CEP" />
-              <input required type="text" placeholder="Rua" />
-            </div>
-            <div>
-              <input required type="text" placeholder="Número" />
-              <label>
-                <input type="text" placeholder="Complemento" />
-                Opcional
-              </label>
-            </div>
-            <div>
-              <input required type="text" placeholder="Bairro" />
-              <input required type="text" placeholder="Cidade" />
-              <input required type="text" placeholder="UF" />
-            </div>
-          </form>
-        </FormContainer>
-      </AddressContainer>
-
-      <ContainerPagament>
-        <div>
-          <CurrencyDollar size={22} />
-          <h1>Pagamento</h1>
-          <p>
-            O pagamento é feito na entrega. Escolha a forma que deseja pagar
-          </p>
-        </div>
-        <SelectPagament>
-          <label htmlFor="debitCard">
-            <input id="debitCard" name="pagament" type="radio" />
-            <CreditCard size={16} />
-            CARTÃO DE CRÉDITO
-          </label>
-
-          <label htmlFor="creditCard">
-            <input id="creditCard" name="pagament" type="radio" />
-            <Bank size={16} />
-            CARTÃO DE DÉBITO
-          </label>
-
-          <label htmlFor="cash">
-            <input id="cash" name="pagament" type="radio" />
-            <Money size={16} />
-            DINHEIRO
-          </label>
-        </SelectPagament>
-      </ContainerPagament>
+      <FormPayment />
 
       <ConfirmOrden>
         <h1>Cafés selecionados</h1>
