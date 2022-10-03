8b0f586a2191355f5ad0ebeef233681fa80a9be3
Martin <escorpmartin97@gmail.com>
2022-10-03 16:48:49 -0300

    feat: customiza erros

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -32,14 +32,16 @@ import {
 import { toast } from 'react-toastify'
 
 const newCoffeeFormValidateSchema = zod.object({
-  cep: zod.string().max(8).min(8, 'Informe o CEP'),
+  cep: zod.string().max(8).min(8, 'Informe um CEP valido'),
   rua: zod.string().min(5, 'Informe a rua'),
   numero: zod.string().min(1, 'Informe o número'),
-  bairro: zod.string().min(5, 'Informe o bairro'),
+  bairro: zod.string().min(3, 'Informe o bairro'),
   cidade: zod.string().min(3, 'Informe o cidade'),
   uf: zod.string().max(2).min(2, 'Informe o bairro'),
   complemento: zod.string().optional(),
-  payment: zod.string(),
+  payment: zod.string({
+    invalid_type_error: 'Debe informar método de pagamento',
+  }),
 })
 
 export function Checkout() {
